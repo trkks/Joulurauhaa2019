31cb12f5030134244a51fec075d016f8c5a7e8bf
Juho Tarkkanen <juho.tarkkanen@hotmail.fi>
2019-12-17 13:49:22 +0200

    Initial commit

---
+++ Pukki.cs
@@ -0,0 +1,18 @@
+using Pipo;
+
+namespace Pipo 
+{
+    class Pukki
+    {
+        const string tag = "pukki";
+
+        SquareCollFacer squcof;
+        SpriteAnimater spranim;
+        
+        public void Swing()
+        {
+        }
+
+
+    }
+}
---
+++ SpriteAnimater.cs
@@ -0,0 +1,12 @@
+using Microsoft.Xna.Graphics:
+
+namespace Pipo 
+{
+    class SpriteAnimater
+    {
+        //readonly int id;
+        Texture2D sprite;
+        Texture2D[] animationFrames;
+        bool isAnimating;
+    }
+}
